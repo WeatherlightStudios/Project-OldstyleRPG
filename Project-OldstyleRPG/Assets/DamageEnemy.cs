de8a4cfcf1f112dc2ea313c9572e7b6af8f1b86b
Cogno <cognokill@gmail.com>
2018-05-06 11:28:34 +0200

    Attack Behaviour
    Enemy import
    Sword Damage
    Sword KnockBack
    Damage Color over time
    Enemy movement
    Enemy animation

---
+++ Project-OldstyleRPG/Assets/DamageEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEnemy : MonoBehaviour {
+
+	public int damage;
+	public float force;
+	public Color damaged;
+	
+	/// <summary>
+	/// OnTriggerEnter is called when the Collider other enters the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Enemy"){
+			other.GetComponent<EnemyBehaviour>().health -= damage;
+			other.GetComponent<SpriteRenderer>().material.color = damaged;
+			other.GetComponent<EnemyBehaviour>().ColorTimer(0.5f);
+			Vector3 enemyDir = other.transform.position - transform.parent.position;
+			enemyDir = Vector3.ProjectOnPlane(enemyDir, Vector3.up).normalized;
+			enemyDir += Vector3.up * 0.4f;
+			other.GetComponent<Rigidbody>().AddForce(enemyDir * force);
+		}
+	}
+}
---
+++ Project-OldstyleRPG/Assets/EnemyBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+	
+	public GameObject target;
+	public GameObject projectile;
+	public Color fine;
+	
+	public float speed;
+	
+	public int health;
+	
+	private Vector3 direction;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 targetDir = target.transform.position - transform.position;
+		targetDir = Vector3.ProjectOnPlane(targetDir, Vector3.up).normalized;
+		
+		transform.position += targetDir * speed * Time.deltaTime;
+		direction = targetDir;
+		
+		if(health <= 0){
+			this.gameObject.SetActive(false);
+		}
+	}
+	
+	public void ColorTimer(float time){
+		StartCoroutine(RepaintTimer(time));
+	}
+	
+	IEnumerator RepaintTimer(float time){
+		yield return new WaitForSeconds(time);
+		this.GetComponent<SpriteRenderer>().material.color = fine;
+	}
+}
