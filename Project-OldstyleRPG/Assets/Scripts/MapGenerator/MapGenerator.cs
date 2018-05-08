8988a232aacf61d591400d136c978bef53ef61ba
Giuliano Gyoshev <darkjeson55@abv.bg>
2018-05-08 15:09:16 +0300

    FInalmente Funziona Generazione dell albero

--- Project-OldstyleRPG/Assets/Scripts/MapGenerator/MapGenerator.cs
+++ Project-OldstyleRPG/Assets/Scripts/MapGenerator/MapGenerator.cs
@@ -2,42 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//classe foglia dell BSP
 public class Room
 {
+	//posizione in basso a sinistra dell'area
 	public float min_posX, min_posY;
+	//posizione in alto a destra dell'area
 	public float max_posX, max_posY;
 
+	//figlio a sinistra
 	Room LeftChildren;
+	//figlio a destra
 	Room RightChildren;
 
+	//il padre di questa foglia
 	Room m_parent;
 
+	// la grandezza minima che puo avere un'area
 	public int min_sizeDivision = 4;
+	//spazziatura della funzione Random nella generazione della divisione X o Y
+	public float RandomTreshold;
+	//percentuale di spazziatura tra altezza e larghezza 
+	public float PercentageTreshold;
 
+	//inizializzazione della classe
 	public Room()
 	{
 
 	}
 
+	//Funzione che suddivide l'albero
 	public void Subdivide()
 	{
-			
+		//controllo per vedere se la stanza sia piu piccola della grandezza minima consentita, e quindi vedere se dividerla nuovamente		
 		if((this.max_posX - this.min_posX) >= min_sizeDivision && (this.max_posY - this.min_posY) >= min_sizeDivision)
 		{
+			//inizializzazione variabili di divisione
 			float divX = 0;
-			float direction = Random.Range(0, 100);
+			float divY = 0;
+
+			//generazione della direzione nella quale verra divisa la stanza
+			float direction = Random.Range(0, 2);
+
+			//inizializzazione delle variabili figlie
 			Room a = new Room();
 			Room b = new Room();
-			divX = Random.Range(this.min_posX, this.max_posX);
-			
-				float divY = 0;
-				divY = (Random.Range(this.min_posY, this.max_posY));
 
+			//check per vedere se la larghezza sia piu grande dell altezza
+			//se e piu grande si divide in verticale
+			//se piu piccolo si divide in horizontale
+			if ((this.max_posX - this.min_posX) > (this.max_posY - this.min_posY) && (this.max_posX - this.min_posX) / (this.max_posY - this.min_posY)  >= 1.5)
+            	direction = 0;
+        	else if ((this.max_posY - this.min_posY) > (this.max_posX - this.min_posX) && (this.max_posY - this.min_posY) / (this.max_posX - this.min_posX) >= 1.5)
+            	direction = 1;
 
-			if(direction % 2 == 0)
+			//genera la posiione x e y della divisione usando la spazziatura
+			divX = Random.Range(0 + RandomTreshold, (this.max_posX - this.min_posX) - RandomTreshold);
+			divY = (Random.Range(0 + RandomTreshold, this.max_posY - this.min_posY) - RandomTreshold);
+
+				
+			//che tipo di direzzione e uscita dall random
+			if(direction == 0)
 			{
 
-
+				//divisione verticale
 				a.min_posX = min_posX;
 				a.min_posY = min_posY;
 
@@ -53,7 +81,7 @@ public class Room
 			}
 			else
 			{
-				
+				//divisione horizontale
 				a.min_posX = min_posX;
 				a.min_posY = min_posY;
 
@@ -67,11 +95,22 @@ public class Room
 				b.max_posY = max_posY;
 			}
 
+			//passaggio delle variabili padre ai figli
+			a.RandomTreshold = RandomTreshold;
+			a.min_sizeDivision = min_sizeDivision;
+			a.PercentageTreshold = PercentageTreshold;
+			
+			b.RandomTreshold = RandomTreshold;
+			b.min_sizeDivision = min_sizeDivision;
+			b.PercentageTreshold = PercentageTreshold;
+
+			//aggiunta dei figli
 			LeftChildren = a;
 			RightChildren = b;
-			if(LeftChildren == null);
+
+			if(LeftChildren != null)
 				LeftChildren.Subdivide();
-			if(RightChildren == null);
+			if(RightChildren != null)
 				RightChildren.Subdivide();
 		}
 
@@ -79,9 +118,10 @@ public class Room
 
 	public void FirstDepth()
 	{
+
+		//disegno dell debug
 		if(LeftChildren != null)
 		{
-
 			Gizmos.DrawWireCube(new Vector3((LeftChildren.min_posX + LeftChildren.max_posX) / 2.0f,0,(LeftChildren.min_posY + LeftChildren.max_posY) / 2.0f) , new Vector3(LeftChildren.max_posX -  LeftChildren.min_posX,0,LeftChildren.max_posY -LeftChildren.min_posY));
 			LeftChildren.FirstDepth();
 		}
@@ -99,6 +139,9 @@ public class Room
 public class MapGenerator : MonoBehaviour 
 {
 	public int dungeonSizeX,  dungeonSizeY;
+	public int m_min_sizeDivision;
+	public float m_PercentageTreshold;
+	public float m_randomTreshold;
 
 	Room m_root = new Room();
 	
@@ -109,15 +152,15 @@ public class MapGenerator : MonoBehaviour
 	
 	void OnDrawGizmos()
 	{
-		m_root.Subdivide();
-		m_root.max_posX = dungeonSizeX;
-		m_root.max_posY = dungeonSizeY;
-
 		m_root.min_posX = 0;		
 		m_root.min_posY = 0;		
+		m_root.min_sizeDivision = m_min_sizeDivision;
+		m_root.PercentageTreshold = m_PercentageTreshold;
+		m_root.max_posX = dungeonSizeX;
+		m_root.max_posY = dungeonSizeY;
+		m_root.RandomTreshold = m_randomTreshold;
+		m_root.Subdivide();
 
-		Gizmos.color = Color.green;
-		Gizmos.DrawWireCube(new Vector3(m_root.min_posX,0,m_root.min_posY) , new Vector3(m_root.max_posX,0,m_root.max_posY));
 		Gizmos.color = Color.red;
 		m_root.FirstDepth();
 
