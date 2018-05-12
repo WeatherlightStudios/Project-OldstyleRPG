5d9e21e0c4395951a1e08fe3b45c2d72651c5417
Cogno <cognokill@gmail.com>
2018-05-12 12:16:45 +0200

    DungeonGround
    instantiated ground of the dungeon

--- Project-OldstyleRPG/Assets/Scripts/MapGenerator/MapGenerator.cs
+++ Project-OldstyleRPG/Assets/Scripts/MapGenerator/MapGenerator.cs
@@ -37,13 +37,23 @@ public class Leaf
 	public float RandomTreshold;
 	//percentuale di spazziatura tra altezza e larghezza 
 	public float PercentageTreshold;
-
+	
+	public GameObject groundTile;
+	
+	public static List<Vector3> positions = new List<Vector3>();
+	public static List<Vector2> sizes = new List<Vector2>();
+	
 	//inizializzazione della classe
 	public Leaf()
 	{
 
 	}
-
+	
+	public void getList(out List<Vector3> _positions, out List<Vector2> _size){
+		_positions = positions;
+		_size = sizes;
+	}
+	
 	//Funzione che suddivide l'albero
 	public void Subdivide()
 	{
@@ -51,11 +61,11 @@ public class Leaf
 		if((this.max_posX - this.min_posX) >= min_sizeDivision && (this.max_posY - this.min_posY) >= min_sizeDivision)
 		{
 			//inizializzazione variabili di divisione
-			float divX = 0;
-			float divY = 0;
+			int divX = 0;
+			int divY = 0;
 
 			//generazione della direzione nella quale verra divisa la stanza
-			float direction = Random.Range(0, 2);
+			int direction = Random.Range(0, 2);
 
 			//inizializzazione delle variabili figlie
 			Leaf a = new Leaf();
@@ -64,14 +74,14 @@ public class Leaf
 			//check per vedere se la larghezza sia piu grande dell altezza
 			//se e piu grande si divide in verticale
 			//se piu piccolo si divide in horizontale
-			if ((this.max_posX - this.min_posX) > (this.max_posY - this.min_posY) && (this.max_posX - this.min_posX) / (this.max_posY - this.min_posY)  >= 1.5)
+			if ((this.max_posX - this.min_posX) > (this.max_posY - this.min_posY) && (this.max_posX - this.min_posX) / (this.max_posY - this.min_posY)  >= PercentageTreshold)
             	direction = 0;
-        	else if ((this.max_posY - this.min_posY) > (this.max_posX - this.min_posX) && (this.max_posY - this.min_posY) / (this.max_posX - this.min_posX) >= 1.5)
+        	else if ((this.max_posY - this.min_posY) > (this.max_posX - this.min_posX) && (this.max_posY - this.min_posY) / (this.max_posX - this.min_posX) >= PercentageTreshold)
             	direction = 1;
 
 			//genera la posiione x e y della divisione usando la spazziatura
-			divX = Random.Range(0 + RandomTreshold, (this.max_posX - this.min_posX) - RandomTreshold);
-			divY = (Random.Range(0 + RandomTreshold, this.max_posY - this.min_posY) - RandomTreshold);
+			divX = (int)Random.Range(0 + RandomTreshold, (this.max_posX - this.min_posX) - RandomTreshold);
+			divY = (int)Random.Range(0 + RandomTreshold, (this.max_posY - this.min_posY) - RandomTreshold);
 
 				
 			//che tipo di direzione e uscita dall random
@@ -118,18 +128,20 @@ public class Leaf
 			a.RandomTreshold = RandomTreshold;
 			a.min_sizeDivision = min_sizeDivision;
 			a.PercentageTreshold = PercentageTreshold;
+			a.groundTile = groundTile;
 			
 			b.RandomTreshold = RandomTreshold;
 			b.min_sizeDivision = min_sizeDivision;
 			b.PercentageTreshold = PercentageTreshold;
+			b.groundTile = groundTile;
 
 			//aggiunta dei figli
 			LeftChildren = a;
 			RightChildren = b;
 
 
-				LeftChildren.Subdivide();
-				RightChildren.Subdivide();
+			LeftChildren.Subdivide();
+			RightChildren.Subdivide();
 		}
 
 	}
@@ -167,18 +179,32 @@ public class Leaf
 		float first_TR_Y = min_pos.y + ((max_pos.y - min_pos.y) / 3.0f) * 2.0f;
 		float second_TR_Y = max_pos.y;
 
-		Vector2 room_pos_bottom_Left = new Vector2(Random.Range(first_BL_X, second_BL_X), Random.Range(first_BL_Y, second_BL_Y));
-		Vector2 room_pos_top_right = new Vector2(Random.Range(first_TR_X, second_TR_X ), Random.Range(first_TR_Y , second_TR_Y));
+		Vector2 room_pos_bottom_Left = new Vector2((int)Random.Range(first_BL_X, second_BL_X), (int)Random.Range(first_BL_Y, second_BL_Y));
+		Vector2 room_pos_top_right = new Vector2((int)Random.Range(first_TR_X, second_TR_X ), (int)Random.Range(first_TR_Y , second_TR_Y));
 		
 		m_room.BL_pos = room_pos_bottom_Left;
 		m_room.TR_pos = room_pos_top_right;
-
-		Gizmos.color = Color.blue;
+		
+		//TODO: put room here
+		
+		//Gizmos.color = Color.blue;
 
 		Vector3 position = new Vector3((m_room.BL_pos.x + m_room.TR_pos.x) / 2.0f,0,(m_room.BL_pos.y + m_room.TR_pos.y) / 2.0f);
 		Vector3 size = new Vector3((m_room.TR_pos.x - m_room.BL_pos.x), 0 ,(m_room.TR_pos.y - m_room.BL_pos.y));
-
-		Gizmos.DrawCube(position, size);
+		
+		Vector2 size2D = new Vector2(size.x, size.z);
+		
+		/*
+		GameObject newRoom = groundTile;
+		newRoom.transform.position = position;
+		newRoom.GetComponentInChildren<SpriteRenderer>().size = size2D;
+		 */
+		
+		positions.Add(position);
+		sizes.Add(size2D);
+		
+		//GameObject ground = Instantiate(groundTile, position, Quaternion.identity);
+		//Gizmos.DrawCube(position, size);
 
 		isLastLeaf = true;
 	}
@@ -194,7 +220,7 @@ public class Leaf
 				RightChildren.connectRooms();
 
 
-			Gizmos.color = Color.green;
+			//Gizmos.color = Color.green;
 
 			float Acenter_X = (LeftChildren.min_posX + LeftChildren.max_posX) / 2.0f;
 			float Acenter_Y = (LeftChildren.min_posY + LeftChildren.max_posY) / 2.0f;
@@ -204,7 +230,19 @@ public class Leaf
 
 			Vector3 from = new Vector3(Acenter_X, 0, Acenter_Y);
 			Vector3 to = new Vector3(Bcenter_X, 0, Bcenter_Y);
-			Gizmos.DrawLine(from, to);
+			//Gizmos.DrawLine(from, to);
+			
+			//Debug.Log("from: " + from + " to: " + to);
+			
+			Vector3 centerPos = (from + to) / 2;
+			Vector2 size = new Vector2(to.x - from.x, to.z - from.z);
+			if(size.x == 0)
+				size.x = 0.5f;
+			if(size.y == 0)
+				size.y = 0.5f;
+			
+			positions.Add(centerPos);
+			sizes.Add(size);
 
 			if(!RightChildren.isHorizontalDiv)
 			{
@@ -243,12 +281,12 @@ public class Leaf
 		//disegno dell debug
 		if(LeftChildren != null)
 		{
-			Gizmos.DrawWireCube(new Vector3((LeftChildren.min_posX + LeftChildren.max_posX) / 2.0f,0,(LeftChildren.min_posY + LeftChildren.max_posY) / 2.0f) , new Vector3(LeftChildren.max_posX -  LeftChildren.min_posX,0,LeftChildren.max_posY -LeftChildren.min_posY));
+			//Gizmos.DrawWireCube(new Vector3((LeftChildren.min_posX + LeftChildren.max_posX) / 2.0f,0,(LeftChildren.min_posY + LeftChildren.max_posY) / 2.0f) , new Vector3(LeftChildren.max_posX -  LeftChildren.min_posX,0,LeftChildren.max_posY -LeftChildren.min_posY));
 			LeftChildren.FirstDepth();
 		}
 		if(RightChildren != null)
 		{
-			Gizmos.DrawWireCube(new Vector3((RightChildren.min_posX + RightChildren.max_posX) / 2.0f,0,(RightChildren.min_posY + RightChildren.max_posY) / 2.0f ) , new Vector3(RightChildren.max_posX - RightChildren.min_posX,0,RightChildren.max_posY -RightChildren.min_posY));
+			//Gizmos.DrawWireCube(new Vector3((RightChildren.min_posX + RightChildren.max_posX) / 2.0f,0,(RightChildren.min_posY + RightChildren.max_posY) / 2.0f ) , new Vector3(RightChildren.max_posX - RightChildren.min_posX,0,RightChildren.max_posY -RightChildren.min_posY));
 			RightChildren.FirstDepth();
 		}
 
@@ -263,16 +301,47 @@ public class MapGenerator : MonoBehaviour
 	public int m_min_sizeDivision;
 	public float m_PercentageTreshold;
 	public float m_randomTreshold;
-
+	
+	public GameObject groundTile;
+	
 	Leaf m_root = new Leaf();
 	
+	public static List<Vector3> positions;
+	public static List<Vector2> sizes;
 
 	void Start()
 	{
+		BSPDungeon();
 	}
 	
 	void OnDrawGizmos()
 	{
+		
+	}
+	
+	void BSPDungeon(){
+		
+		SetupRoot();
+		m_root.Subdivide();
+
+		//Gizmos.color = Color.red;
+		m_root.FirstDepth();
+		m_root.CheckRoom();
+		m_root.connectRooms();
+		
+		m_root.getList(out positions, out sizes);
+		for(int i = 0; i < positions.Count; i++){
+			GameObject newRoom = Instantiate(groundTile, positions[i], Quaternion.identity);
+			newRoom.GetComponentInChildren<SpriteRenderer>().size = sizes[i];
+		}
+	}
+	
+	static void Test(){
+		
+	}
+	
+	void SetupRoot(){
+		
 		m_root.min_posX = 0;		
 		m_root.min_posY = 0;		
 		m_root.min_sizeDivision = m_min_sizeDivision;
@@ -280,12 +349,7 @@ public class MapGenerator : MonoBehaviour
 		m_root.max_posX = dungeonSizeX;
 		m_root.max_posY = dungeonSizeY;
 		m_root.RandomTreshold = m_randomTreshold;
-		m_root.Subdivide();
-
-		Gizmos.color = Color.red;
-		m_root.FirstDepth();
-		m_root.CheckRoom();
-		m_root.connectRooms();
-
+		
+		m_root.groundTile = this.groundTile;
 	}
 }
