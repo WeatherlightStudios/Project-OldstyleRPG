32c2e45328309d2e9be550310e686c65eca87cb3
Cogno <cognokill@gmail.com>
2018-05-14 09:54:13 +0200

    Player Prefab Recover

---
+++ Project-OldstyleRPG/Assets/HealthBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBehaviour : MonoBehaviour {
+	
+	public int health;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void Damage(int damage){
+		health -= damage;
+	}
+}
--- Project-OldstyleRPG/Assets/Scripts/EnemyBehaviour.cs
+++ Project-OldstyleRPG/Assets/Scripts/EnemyBehaviour.cs
@@ -8,6 +8,8 @@ public class EnemyBehaviour : MonoBehaviour {
 	public GameObject projectile;
 	public Color fine;
 	
+	public int damage;
+	
 	public float speed;
 	
 	public int health;
@@ -41,5 +43,17 @@ public class EnemyBehaviour : MonoBehaviour {
 	IEnumerator RepaintTimer(float time){
 		yield return new WaitForSeconds(time);
 		this.GetComponent<SpriteRenderer>().material.color = fine;
+	}
+	
+	/// <summary>
+	/// OnCollisionEnter is called when this collider/rigidbody has begun
+	/// touching another rigidbody/collider.
+	/// </summary>
+	/// <param name="other">The Collision data associated with this collision.</param>
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag == "Player"){
+			other.gameObject.GetComponent<HealthBehaviour>().Damage(damage);
+		}
 	}
 }
